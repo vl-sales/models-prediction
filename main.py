7e54a609ba3d1561ead8c54ff0af83f6b3cef7f9
Vinicius Sales <vinicius.sales@stratesys-ts.com>
2023-05-21 20:24:23 -0300

    set new port

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from src.app import create_app
-import os
+import sys
 import logging
-from waitress import serve
 logging.basicConfig(level=logging.DEBUG)
 
 app = create_app()
 
 if __name__ == '__main__':
-    port = os.getenv('PORT') if os.getenv('PORT') else "8000"
-    logging.debug("Starting SERVER on port: " + port)
-    serve(app, host="0.0.0.0", port=port)
+    args = sys.argv[1:]
+    if len(args) < 1:
+        args.append('8080')
+    print(args)
+
+    logging.debug("Starting SERVER on port: " + args[0])
+    app.run(port=args[0], host='0.0.0.0')
